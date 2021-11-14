54128fc629e288299cea8d45d45034175a18def3
Guilherme Vieira <guivpw68@gmail.com>
2021-11-14 02:18:26 -0300

    chore: add webpack configurations for material ui styled components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+const withTM = require('next-transpile-modules')([
+	'@mui/material',
+	'@mui/lab',
+	'@mui/system',
+	'@mui/icons-material',
+	'@mui/styled-engine'
+])
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-	reactStrictMode: true
-}
+module.exports = withTM({
+	reactStrictMode: true,
+	webpack: config => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'@mui/styled-engine': '@mui/styled-engine-sc'
+		}
+
+		return config
+	}
+})
