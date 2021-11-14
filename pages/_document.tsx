fcf883944d392ab844d85ea070b3da9b1e1fa7bc
Guilherme Vieira <guivpw68@gmail.com>
2021-11-14 03:41:41 -0300

    feat: add nextjs document config with emotion recommended configuration

---
+++ pages/_document.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+
+import createEmotionServer from '@emotion/server/create-instance'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import createEmotionCache from '../src/config/emotionCache.config'
+import { materialTheme } from '../src/styles/material-theme'
+
+export default class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="pt-BR">
+				<Head>
+					<meta name="theme-color" content={materialTheme.palette.primary.main} />
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+MyDocument.getInitialProps = async ctx => {
+	const originalRenderPage = ctx.renderPage
+
+	const cache = createEmotionCache()
+	const { extractCriticalToChunks } = createEmotionServer(cache)
+
+	ctx.renderPage = () =>
+		originalRenderPage({
+			// eslint-disable-next-line react/display-name
+			enhanceApp: (App: any) => props => <App emotionCache={cache} {...props} />
+		})
+
+	const initialProps = await Document.getInitialProps(ctx)
+	const emotionStyles = extractCriticalToChunks(initialProps.html)
+	const emotionStyleTags = emotionStyles.styles.map(style => (
+		<style
+			data-emotion={`${style.key} ${style.ids.join(' ')}`}
+			key={style.key}
+			dangerouslySetInnerHTML={{ __html: style.css }}
+		/>
+	))
+
+	return {
+		...initialProps,
+		styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags]
+	}
+}
