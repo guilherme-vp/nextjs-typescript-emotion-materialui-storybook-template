b46cbcb104b8ac3311c557db1c19afbb4128f619
Guilherme Vieira <guivpw68@gmail.com>
2021-11-14 02:24:34 -0300

    stories: add styles wrapper to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { StylesWrapper } from '../styles/Wrapper.styles'
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
@@ -7,3 +9,11 @@ export const parameters = {
 		}
 	}
 }
+
+export const decorators = [
+	Story => (
+		<StylesWrapper>
+			<Story />
+		</StylesWrapper>
+	)
+]
